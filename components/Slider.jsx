396951b693731192a5db3b440628215d0f1a01df
jairalcon <98365593+jairalcon@users.noreply.github.com>
2023-01-24 18:54:38 -0600

    codemod image to legacy image

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/legacy/image";
 import React, { useState } from 'react';
 import { SliderData } from './SliderData';
 import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
